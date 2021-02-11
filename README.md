c7c74cf114bd3f29abb433d85fe3d8d3dc15c659
joed <joed@Joes-MBP-2.lan>
2021-02-11 21:46:30 +0000

    Initial version of App

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # aib-cmom-tech-test
 sample project for AIB 
+
+Instructions
+
+1. clone the repo to yopur local env
+
+2. cd to the 'coin-gecko' directory in the cmd window
+
+3. run command 'npm start'
+
+4. in your browser go to 'http://localhost:3000'
--- coin-gecko/src/App.js
+++ coin-gecko/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [coinMarketsData, setCoinMarketsData] = useState(null);
+  const [coinData, setCoinData] = useState({ name: '', symbol: '' });
+  const [currentCoinID, setCurrentCoinID] = useState('');
+
+  function onCurrentCoinChange(id) {
+    setCurrentCoinID(id);
+    fetch(`https://api.coingecko.com/api/v3/coins/${id}`)
+      .then((response) => response.json())
+      .then(setCoinData);
+  }
+
+  useEffect(() => {
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=10&page=1')
+      .then((response) => response.json())
+      .then(setCoinMarketsData);
+  }, []);
+
+  if (coinMarketsData) {
+    return (
+      <>
+        <div className="card">
+          <header className="App-header card-header">
+            Coin Gecko
+          </header>
+
+          <div className="card-body">
+            <Coin coinData={coinData} />
+            <Table tableData={coinMarketsData} onCurrentRowChange={onCurrentCoinChange} />
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return <div>No coin data available</div>
+}
+
+function Coin(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="w-50 mx-auto p-3 bg-success text-center text-white">{props.coinData.symbol} - {props.coinData.name}</div>
+  )
+}
+
+function Table(props) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Image</th>
+          <th scope="col">Name</th>
+          <th scope="col">Symbol</th>
+          <th scope="col">Symbol</th>
+          <th scope="col">High 24hr price</th>
+          <th scope="col">Low 24hr price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.tableData.map((rowData) =>
+          <tr key={rowData.id} onClick={() => props.onCurrentRowChange(rowData.id)}>
+            <td><img src={rowData.image} height="30px" /></td>
+            <td>{rowData.name}</td>
+            <td>{rowData.symbol}</td>
+            <td>{rowData.current_price}</td>
+            <td>{rowData.high_24h}</td>
+            <td>{rowData.low_24h}</td>
+          </tr>)}
+      </tbody>
+    </table>
+  )
 }
 
 export default App;
